b483fc8cc3a8c8ce8679abd65d3ce7975c07089e
risan <risanbagja@gmail.com>
2018-01-07 08:36:44 +0100

    :white_check_mark: Add test

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,7 @@
 import SwedenPrayerTimes from '../src';
 
-test('there is no actual tests being performed', () => {
-  expect(1 + 1).toBe(2);
+const PRAYER_TIMES_URI = 'http://www.islamiskaforbundet.se/wp-content/plugins/bonetider/Bonetider_Widget.php';
+
+test('can get URI', () => {
+  expect(SwedenPrayerTimes.URI).toBe(PRAYER_TIMES_URI);
 });
