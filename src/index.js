5f45ae7f12e9c98f6ed35cf4c3ef3f0e3898a7be
risan <risanbagja@gmail.com>
2018-11-10 08:58:26 +0100

    refactor: extract the http request function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,32 @@ const parseDate = require("date-fns/parse");
 const qs = require("qs");
 
 /**
+ * Send request to retrieve prayer times.
+ *
+ * @param {String} city
+ * @param {Date} date
+ * @return {Object}
+ */
+const request = async (city, date) => {
+  const url =
+    "https://www.islamiskaforbundet.se/wp-content/plugins/bonetider/Bonetider_Widget.php";
+
+  const params = {
+    ifis_bonetider_widget_city: `${city}, SE`,
+    ifis_bonetider_widget_date: formatDate(date, "YYYY-MM-DD")
+  };
+
+  const response = await axios.post(url, qs.stringify(params), {
+    headers: {
+      "content-type": "application/x-www-form-urlencoded"
+    },
+    responseType: "text"
+  });
+
+  return response;
+};
+
+/**
  * Get the prayer times.
  *
  * @param {String} city
@@ -18,20 +44,7 @@ const getPrayerTimes = async (city, date = new Date()) => {
     throw new Error("The given date is invalid.");
   }
 
-  const url =
-    "https://www.islamiskaforbundet.se/wp-content/plugins/bonetider/Bonetider_Widget.php";
-
-  const params = {
-    ifis_bonetider_widget_city: `${city}, SE`,
-    ifis_bonetider_widget_date: formatDate(parsedDate, "YYYY-MM-DD")
-  };
-
-  const { data } = await axios.post(url, qs.stringify(params), {
-    headers: {
-      "content-type": "application/x-www-form-urlencoded"
-    },
-    responseType: "text"
-  });
+  const { data } = await request(city, parsedDate);
 
   const times = data.match(/\d{2}:\d{2}/gm);
 
