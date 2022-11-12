9090230d1959d899eedf689d0f00d6e941b23459
Yusufware <yusuf.hamawandiwork@gmail.com>
2022-11-12 12:20:28 +0100

    Added comments and documentation to code base

--- src/capitalize-first-letter.js
+++ src/capitalize-first-letter.js
@@ -4,7 +4,17 @@
  * @param {String} str
  * @return {String}
  */
+
+// str.charAt(char_index_within_string) -> gets first char(character) from string
+
+// str.toUpperCase() -> Capitalizes string or char(character).
+// In this case the char(character) at index 0 within string.
+
+// + str.slice(1).toLowerCase() -> Makes every character after index 0 lowercase.
+
 const capitalizeFirstLetter = str =>
   str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
+
+//Exports module, allowing use for function outside of current file. 
 module.exports = capitalizeFirstLetter;
--- src/send-request.js
+++ src/send-request.js
@@ -1,6 +1,9 @@
+//Imports request function from send-request.js file
 const request = require("send-request");
+
 const formatDate = require("date-fns/format");
 
+//Imports capitalizeFirstLetter function from capitalize-first-letter.js file
 const capitalizeFirstLetter = require("./capitalize-first-letter");
 
 /**
@@ -11,9 +14,11 @@ const capitalizeFirstLetter = require("./capitalize-first-letter");
  * @return {String}
  */
 const sendRequest = async (city, date) => {
+  //Requested url
   const url =
     "https://www.islamiskaforbundet.se/wp-content/plugins/bonetider/Bonetider_Widget.php";
 
+  //Awaiting post request from website
   const { body } = await request(url, {
     method: "POST",
     body: {
@@ -21,8 +26,9 @@ const sendRequest = async (city, date) => {
       ifis_bonetider_widget_date: formatDate(date, "YYYY-MM-DD")
     }
   });
-
+  // Returns html body
   return body;
 };
 
+//Exports module, allowing use for function outside of current file. 
 module.exports = sendRequest;
